8f22b685496c97d0130454d5f2bc14ca8e5cb3b9
WolfDatka <wolfdatka@gmail.com>
2024-10-13 09:57:09 +0200

    prepared task 3

--- premake5.lua
+++ premake5.lua
@@ -86,3 +86,42 @@ project "Task2-Page22"
         defines {
             "RELEASE",
         }
+
+project "Task3-Page22"
+    location "Task3-Page22"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("%{prj.location}/Bin/" .. outputDir)
+    objdir ("%{prj.location}/Obj/" .. outputDir)
+
+    files {
+        "%{prj.location}/Src/**.cpp",
+        "%{prj.location}/Headers/**.h",
+    }
+
+    includedirs {
+        "%{prj.location}/Headers/",
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        symbols "On"
+        defines {
+            "DEBUG",
+        }
+
+    filter "configurations:FastDebug"
+        optimize "On"
+        defines {
+            "FAST_DEBUG",
+        }
+
+    filter "configurations:Release"
+        optimize "On"
+        defines {
+            "RELEASE",
+        }
